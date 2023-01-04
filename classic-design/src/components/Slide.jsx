b478ecf4be5c3fe92d09b010a47a6dd2111c21c2
shahil-leo <shahilcode@gmail.com>
2023-01-04 13:20:06 +0530

    added the slide in the js

--- classic-design/src/components/Slide.jsx
+++ classic-design/src/components/Slide.jsx
@@ -1,153 +1,51 @@
 import React from 'react';
-import styled from 'styled-components'
-import { Search } from "@mui/icons-material";
-
-const Container = styled.section`
-height: 100vh;
-background-image: url('./Files/markus-spiske-k0rVudBoB4c-unsplash 1.jpg');
-background-repeat: no-repeat;
-background-size:cover;
-background-position:center;
-
-`
-const Wrapper = styled.section`
- height: 55vh;
- padding : 250px 0px 0px 200px ;
- `
-const TitleDiv = styled.div`
-    width: 600px;
-    
-`
-
-const Title = styled.h1`
-    system
-    color: black;
-    font-weight: 700;
-    font-size: 75px;
-    line-height: 102px;
-    font-wei
-    font-wei
-    font-wei
-`
-const Para = styled.p`
-    width: 279px;
-    font-weight: 300;
-    font-size: 17px;
-    margin-top: 30px;
-    margin-bottom: 55px;
-`
-const ButtonDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 250px;
-  justify-content: space-between;
-  /* background-color: black; */
-`
-const Button = styled.button`
- background-color: #EC3B3B;
- color: white;
- border-radius: 10px;
-padding: 10px 20px 10px 20px ;
-border: none;
-`
-const Span = styled.span`
-  color: red;
-`
-const ButtonSecond = styled.button`
-   background-color: transparent;
- border-radius: 10px;
- padding: 10px 30px 10px 30px ;
- border: 1px solid black ;
-`
-// Navbar section
-
-const Nav = styled.div`
-  display: flex;
-  height: auto;
-  justify-content: space-between;
-  padding: 1% 10% 0px 10%;
-  align-items: center;
-`;
-const First = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 370px;
-`;
-const Second = styled.div``;
-const Third = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  gap: 30px;
-  align-items: center;
-`;
-const ButtonFirst = styled.button`
-  width: 140px;
-  height: 45px;
-  &:hover {
-    background-color: #000000;
-    color: white;
-    transition: .3s ease;
-  box-shadow: 0px 0px 5px 0.1px  white;
-
-  }
-  border-radius: 5px;
-  border: none;
-  box-shadow: 0px 0px 5px 0.1px  #00000044;
-`;
-const Input = styled.input`
-  border: 0.2px solid black;
-  border-radius: 5px;
-  padding-left: 35px;
-  width: 250px;
-  height: 45px;
-  position: relative;
-`;
-const Logo = styled.h1`
-  font-weight: 600;
-  font-size: 40px;
-`;
-const Anger = styled.a`
-  
-  text-decoration: none;
-  color: #000000b3;
-`;
-const style = { position: "absolute", marginLeft: "5px" };
+// import background from './Files/backgroundpic.jpg'
 
 function Slide() {
   return (
-
-    <Container>
-      <Nav>
-        <First>
-          <Anger href="shahilkv.com">Categories</Anger>
-          <Anger href="shahilkv.com">Register</Anger>
-          <Anger href="shahilkv.com">Login</Anger>
-        </First>
-        <Second>
-          <Logo>
-            CLASSIC<span style={{ color: "red" }}>.</span>
-          </Logo>
-        </Second>
-        <Third>
-          <Input type="text" placeholder="Search" />
-          <Search style={style} />
-          <ButtonFirst>Cart </ButtonFirst>
-        </Third>
-      </Nav>
-      <Wrapper>
-        <TitleDiv>
-          <Title>The Perfect <Span>Tool</Span>  you need</Title>
-        </TitleDiv>
-        <Para>Simple easy and comfortable</Para>
-        <ButtonDiv>
-          <Button>Buy Now</Button>
-          <ButtonSecond>About</ButtonSecond>
-        </ButtonDiv>
-      </Wrapper>
-    </Container>
+    <section
+      // style={
+      //   {
+      //     backgroundImage: `url(${background})`,
+      //     backgroundPosition: 'center',
+      //     backgroundSize: 'cover',
+      //     backgroundRepeat: 'no-repeat'
+      //   }
+      // } 
+      style={
+        {
+          height: '94vh',
+        }
+      }
+      className='  pr-400 pl-52 pt-10'>
+      {/* navbar section */}
+      <div className='flex flex-row  gap-40 '>
+        <div className='flex flex-row w-1/4 gap-14'>
+          <a className='text-red-400   text-lg w-45' href="shahilkv.com">Categories</a>
+          <a className='text-red-400  text-lg w-45' href="shahilkv.com">Register</a>
+          <a className='text-red-400  text-lg w-45' href="shahilkv.com">Login</a>
+        </div>
+        <div>
+          <h1 className='w-56 font-semibold text-5xl'>CLASSIC<span style={{ color: "red" }}>.</span></h1>
+        </div>
+        <div>
+          {/* <Search className='p-20'/>  */}
+          <input placeholder='Search here' className='rounded-lg border-black text-xl border-2 pl-4 p-2 w-64 ml-20 mr-5' type="text" />
+          <button className='border-black bg-black text-white px-9 py-3 rounded-lg'>Cart</button>
+        </div>
+      </div>
+      {/* end of navbar */}
+      <div>
+        <div className=' w-full '>
+          <h1 className='font-semibold text-8xl mt-48 w-5/12 h-46 leading-snug'>The perfect Tool you need</h1>
+        </div>
+        <p className='opacity-75 pl-2 mt-10 mb-20'>Simple Easy and comfortable</p>
+        <div>
+          <button className='rounded-md px-10 py-4 bg-red-400 mr-10'>Buy Now</button>
+          <button className='rounded-md px-10 py-4 bg-red-400'>About</button>
+        </div>
+      </div>
+    </section>
   )
 }
 

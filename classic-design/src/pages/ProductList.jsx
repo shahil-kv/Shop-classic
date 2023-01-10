3c3725e2c8b047583f5f96a1bfa14b7a41c74dc2
shahil-leo <shahilcode@gmail.com>
2023-01-10 07:49:13 +0530

    added the filtering in the product section for the size and color

--- classic-design/src/pages/ProductList.jsx
+++ classic-design/src/pages/ProductList.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Products from '../components/Products'
+import { useLocation } from 'react-router-dom'
+import { useState } from 'react'
 const Container = styled.div`
     
  `
@@ -31,6 +33,20 @@ const Option = styled.option`
     
   `
 const ProductList = () => {
+   // we are getting the data of our current location werw we are sitting
+   const location = useLocation()
+   // then we are doing the split method to split them
+   const cat = location.pathname.split("/")[2];
+   const [filter, setFilters] = useState({})
+   const handleFilters = (e) => {
+      console.log(`shahil  ${e}`)
+      const value = e.target.value
+      setFilters({
+         ...filter,
+         [e.target.name]: value,
+      });
+   };
+   console.log(filter)
    return (
       <Container>
          <Navbar />
@@ -38,8 +54,8 @@ const ProductList = () => {
          <FilterContainer>
             <Filter>
                <FilterText>Filter Products :</FilterText>
-               <Select>
-                  <Option disabled selected > Color</Option>
+               <Select name='color' onChange={handleFilters}>
+                  <Option disabled  > Color</Option>
                   <Option> White</Option>
                   <Option> Black</Option>
                   <Option> Red</Option>
@@ -47,8 +63,8 @@ const ProductList = () => {
                   <Option> Yellow</Option>
                   <Option> Green</Option>
                </Select>
-               <Select>
-                  <Option disabled selected >Size</Option>
+               <Select name='size' onChange={handleFilters}>
+                  <Option disabled >Size</Option>
                   <Option> XS</Option>
                   <Option> S</Option>
                   <Option> M</Option>

a3fc514698e018a473983de78c3e7329d67a074d
shahil-leo <shahilcode@gmail.com>
2023-01-10 06:53:34 +0530

    added the initial routing for the website

--- classic-design/src/App.jsx
+++ classic-design/src/App.jsx
@@ -1,12 +1,37 @@
 import Home from "./pages/Home";
-// import Login from "./pages/Login";
-// import Product from "./pages/Product";
-// import ProductList from "./pages/ProductList";
-// import Register from "./pages/Register";
-// import Cart from './pages/Cart';
-// import Login from './pages/Login'
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Cart from './pages/Cart';
+
+import {
+  BrowserRouter, Link, Route, Routes
+} from "react-router-dom"
+
 const App = () => {
-  return <Home />
-};
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path="/product" element={<ProductList />} />
+      </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+      </Routes>
+      <Routes>
+        <Route path="/products" element={<Product />} />
+      </Routes>
+      <Routes>
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 export default App;

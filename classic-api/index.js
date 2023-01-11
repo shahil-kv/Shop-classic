8176cae3b17b301fa8f20fa4fc9b27be0e9583b0
shahil-leo <shahilcode@gmail.com>
2023-01-11 06:59:34 +0530

    added the route for the stripe route and imported

--- classic-api/index.js
+++ classic-api/index.js
@@ -2,28 +2,33 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute=require('./routes/user')
-const authRoute=require('./routes/auth')
-const productRoute=require('./routes/product')
-const cartRoute=require('./routes/cart')
+const userRoute = require('./routes/user')
+const authRoute = require('./routes/auth')
+const productRoute = require('./routes/product')
+const cartRoute = require('./routes/cart')
 const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+const cors = require('cors')
 dotenv.config();
 
 mongoose
   .set("strictQuery", false)
   .connect(process.env.MONGO_URL)
   .then(() => console.log('connected to DB'))
-  .catch((error) => console.log('error in database connecting',error))
+  .catch((error) => console.log('error in database connecting', error))
 
+
+app.use(cors())
 //   what we are doing here is seting the api/user and the userRoute for showing 
 // we can use the other routes using the api/user/and then if we put any of the links it will go to userRoute and if there is a link then it will be collected
-  app.use(express.json())
-  // This is the user Router we need to done
-  app.use('/api/users',userRoute);
-  app.use('/api/auth',authRoute );
-  app.use('/api/products',productRoute)
-  app.use('/api/cart',cartRoute)
-  app.use('/api/orders',orderRoute)
-  app.listen(5000, () => {
+app.use(express.json())
+// This is the user Router we need to done
+app.use('/api/users', userRoute);
+app.use('/api/auth', authRoute);
+app.use('/api/products', productRoute)
+app.use('/api/cart', cartRoute)
+app.use('/api/orders', orderRoute)
+app.use('api/checkout', stripeRoute)
+app.listen(5000, () => {
   console.log("Backend server is running on port 5000");
- });
+});

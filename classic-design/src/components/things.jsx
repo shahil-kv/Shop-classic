6817d7ae2a80372e7588121e66e7ef9ea438a0af
shahil-leo <shahilcode@gmail.com>
2023-01-12 09:28:26 +0530

    changed the things section to tailwind and added the things

--- classic-design/src/components/things.jsx
+++ classic-design/src/components/things.jsx
@@ -3,22 +3,30 @@
 const Things = ({ item }) => {
     return (
         <div className=" flex flex-row  pr-60 gap-3 pl-48 mt-44 mb-44">
-            <div className="w-1/3 h-72  text-white rounded bg-red-500 flex flex-col justify-center items-center">
-            <div className="mb-10 w-20 rounded-3xl bg-white h-16"></div>
+            <div className="w-1/3 h-72  text-white rounded bg-red-200 flex flex-col justify-center gap-7 items-center">
+                <div className="ml-10 w-20  rounded-full bg-white h-20 flex flex-col  justify-center items-center">
+                    <img className='w-12 text-center' src={window.location.origin + '/Files/reviewIcon/libre-gui-shipping logo.svg'} alt="ratings" />
+                </div>
                 <h1 className="text-white">Super Fast and Free Delivary</h1>
             </div>
             <div className="w-1/3   text-white flex flex-col gap-2 ">
-                <div className=" w-2/2 h-1/2 bg-green-400 rounded flex items-center gap-10 ">
-                    <div className="ml-10 w-20 rounded-3xl bg-white h-16"> </div>
-                    <h1>Super Fast and Free Delivary</h1>
+                <div className=" w-2/2 h-1/2 bg-red-200 rounded flex items-center gap-10 ">
+                    <div className="ml-10 w-20  rounded-full bg-white h-20 flex flex-col  justify-center items-center">
+                        <img className='w-8 text-center' src={window.location.origin + '/Files/reviewIcon/Google logo.svg'} alt="ratings" />
+                    </div>
+                    <h1>Non Contact Shipping</h1>
                 </div>
-                <div className="bg-red-500 w-2/2 h-1/2 rounded flex items-center">
-                <div className="m-10 w-20 rounded-3xl bg-white h-16"> </div>
-                    <h1 className="text-white">Non Contact shipping</h1>
-             </div>
+                <div className="bg-red-200 w-2/2 h-1/2 rounded flex items-center gap-12">
+                    <div className="ml-10 w-20  rounded-full bg-white h-20 flex flex-col  justify-center items-center">
+                        <img className='w-8 text-center' src={window.location.origin + '/Files/reviewIcon/Budget logo.svg'} alt="ratings" />
+                    </div>
+                    <h1 className="text-white">Money back Gurrenty</h1>
+                </div>
             </div>
-            <div className="w-1/3  rounded bg-black text-white flex flex-col justify-center items-center">
-            <div className="mb-10 w-20 rounded-3xl bg-white h-16 "></div>
+            <div className="w-1/3  rounded bg-red-200 text-white flex flex-col justify-center items-center gap-7">
+                <div className="ml-10 w-20  rounded-full bg-white h-20 flex flex-col  justify-center items-center">
+                    <img className='w-8 text-center' src={window.location.origin + '/Files/reviewIcon/Layer 2.svg'} alt="ratings" />
+                </div>
                 <h1 className="text-white">Super Fast and Free Delivary</h1>
 
             </div>

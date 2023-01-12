6d5b66b0797cd33e672593f4cfe3dcd068706652
shahil-leo <shahilcode@gmail.com>
2023-01-12 06:53:21 +0530

    changed teh review section styling into new tailwind

--- classic-design/src/components/ReviewN.jsx
+++ classic-design/src/components/ReviewN.jsx
@@ -1,165 +1,45 @@
 import React from 'react'
-import styled from 'styled-components'
 
-
-const Fdiv = styled.div`
-  width: 460px;
-  background-color: #ADD8E6;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding-left: 40px;
- border-radius: 10px;
- &:hover {
-     animation: 2s shahil;
-      transition: 5s ease;
-   }
-   @keyframes shahil {
-     0%{
-       scale: 1.1;
-     }
-     100%{
-        scale: 1.1;
-     }
-    
-   }
-
-`
-
-const Container = styled.div`
-  height: 600px;
-  
-`
-
-const Sdiv = styled.div`
-  width: 460px;
-  background-color: #ADD8E6;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding-left: 40px;
- border-radius: 10px;
- &:hover {
-     animation: 2s shahil;
-      transition: 5s ease;
-   }
-   @keyframes shahil {
-     0%{
-       scale: 1.1;
-     }
-     100%{
-        scale: 1.1;
-     }
-    
-   }
-`
-const Tdiv = styled.div`
-  width: 460px;
-  background-color: #ADD8E6;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding-left: 40px;
- border-radius: 10px;
- &:hover {
-     animation: 2s shahil;
-      transition: 5s ease;
-   }
-   @keyframes shahil {
-     0%{
-       scale: 1.1;
-     }
-     100%{
-        scale: 1.1;
-     }
-   }
-`
-const Scontainer = styled.div`
- margin-top: 25px;
- display: flex;
- height: 400px;
- flex-direction: row;
- /* background-color: red; */
- justify-content: center;
- justify-content: space-between;
- margin-left:10%;
- margin-right: 13%;
-`
-
-
-const Text = styled.p`
-   font-size: 20px;
-   font-weight: 400;
-   width: 400px;
-`
-const Name = styled.h5`
-   font-size: 15px;
-   font-weight: 400;
-`
-const About = styled.h6`
-   font-size:  10;
-   font-weight: 100;
-`
-const Image = styled.img`
-  
-`
-const Flexdiv = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-`
-const Namediv = styled.div`
-
-`
-
-const Title = styled.h1`
-     display: flex;
-     flex-direction: row;
-     justify-content: center;
-     margin-top: 130px;
-     margin-bottom: 70px;
-     font-size:34px;
-`
 const ReviewN = () => {
   return (
-    <Container>
-      <Title>What people say about our service</Title>
-      <Scontainer>
-        <Fdiv>
-          <img src="" alt="icons" />
-          <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since </Text>
-          <Flexdiv>
-            <Image src='' alt='shahil' />
-            <Namediv>
-              <Name>Shahil</Name>
-              <About>Software Developer</About>
-            </Namediv>
-          </Flexdiv>
-        </Fdiv>
-        <Sdiv>
-          <img src="s" alt="icons" />
-          <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</Text>
-          <Flexdiv>
-            <Image src='' alt='shahil' />
-            <Namediv>
-              <Name>Lionel Messi</Name>
-              <About>Argentinian Footballer</About>
-            </Namediv>
-          </Flexdiv>
-        </Sdiv>
-        <Tdiv>
-          <img src="s" alt="icons" />
-          <Text>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since</Text>
-          <Flexdiv>
-            <Image src='' alt='shahil' />
-            <Namediv>
-              <Name>Hisham Kannapi</Name>
-              <About>Team lead of kannapi gang</About>
-            </Namediv>
-          </Flexdiv>
-        </Tdiv>
-      </Scontainer>
-    </Container>
+    <div className='h-1/4'>
+      <h1 className='flex flex-row justify-center mt-32 mb-32 text-4xl'>What people say about our service</h1>
+      <div className='mt-23 flex h-96 flex-row justify-center gap-16 ml-52 mr-52 mb-52'>
+        <div className='w-9/12 flex flex-col justify-evenly pl-10  rounded-3xl bg-blue-200 Fdiv hover:animate-shahil p-5 item'>
+          <img className='w-24 text-center' src={window.location.origin + '/Files/Stars.svg'} alt="ratings" />
+          <p className='w-96 font-normal text-lg'>I just wanted to share a quick note and let you know that you guys do a really good job. I’m glad I decided to work with you. </p>
+          <div className='flex flex-row gap-36'>
+            <img className='w-14 rounded-full h-14 ' src={window.location.origin + '/Files/christopher-campbell-rDEOVtE7vOs-unsplash.jpg'} alt='shahil' />
+            <div>
+              <h5 className='text-xl font-medium w-6/6'>Shahil</h5>
+              <h6 className='text-sm font-light'>Software Developer</h6>
+            </div>
+          </div>
+        </div>
+        <div className='w-9/12 flex flex-col justify-evenly pl-10 rounded-3xl bg-blue-200 p-5'>
+          <img className='w-24 text-center' src={window.location.origin + '/Files/Stars.svg'} alt="ratings" />
+          <p className='w-96 font-normal text-lg'>“Wow. I just updated my site and it was SO SIMPLE. I am blown away. You guys truly kick ass. Thanks for being so awesome. High fives!”</p>
+          <div className='flex flex-row  gap-36'>
+            <img className='w-14 rounded-full h-14 ' src={window.location.origin + '/Files/mediamodifier-0UBg7jhsSxs-unsplash.jpg'} alt='shahil' />
+            <div>
+              <h5 className='text-xl font-medium w-6/6'>Lionel Messi</h5>
+              <h6 className='text-sm font-light'>Argentinian Footballer</h6>
+            </div>
+          </div>
+        </div>
+        <div className='w-9/12 flex flex-col justify-evenly pl-10 rounded-3xl bg-blue-200 p-5'>
+          <img className='w-24 text-center' src={window.location.origin + '/Files/Stars.svg'} alt="ratings" />
+          <p className='w-96 font-normal text-lg'>“I just wanted to share a quick note and let you know that you guys do a really good job. I’m glad I decided to work with you. It’s really great </p>
+          <div className='flex flex-row  gap-36'>
+            <img className='w-14 rounded-full h-14 ' src={window.location.origin + '/Files/jurica-koletic-7YVZYZeITc8-unsplash.jpg'} alt='shahil' />
+            <div>
+              <h5 className='text-xl font-medium w-6/6'>Hisham Kannapi</h5>
+              <h6 className='text-sm font-light'>Team lead of kannapi gang</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 

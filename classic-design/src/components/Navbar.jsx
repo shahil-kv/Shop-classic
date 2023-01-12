b6aec255b67e4084c30d448a37d09d80e74b07fd
shahil-leo <shahilcode@gmail.com>
2023-01-12 07:34:07 +0530

    added the navbar section

--- classic-design/src/components/Navbar.jsx
+++ classic-design/src/components/Navbar.jsx
@@ -1,94 +1,36 @@
-import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { ShoppingCartOutlined } from "@mui/icons-material";
 import { Badge } from "@mui/material";
 import React from "react";
-import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom'
-// this is the styling of the navbar
-// styled components  is used in it
 
-const Container = styled.div`
-  display: flex;
-  height: auto;
-  justify-content: space-between;
-  background-color: white;
-  padding: 1% 10% 0px 10%;
-  align-items: center;
-`;
-const First = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  /* background-color: red; */
-  width: 370px;
-`;
-const Second = styled.div``;
-const Third = styled.div`
-  display: flex;
-  /* background-color: green; */
-  flex-direction: row;
-  justify-content: space-between;
-  gap: 30px;
-  align-items: center;
-`;
-const Button = styled.button`
-  width: 140px;
-  /* background-color: yellow; */
-  height: 45px;
-  &:hover {
-    background-color: #000000;
-    color: white;
-    transition: .3s ease;
-  box-shadow: 0px 0px 5px 0.1px  white;
-
-  }
-  border-radius: 5px;
-  border: none;
-  box-shadow: 0px 0px 5px 0.1px  #00000044;
-`;
-const Input = styled.input`
-  border: 0.2px solid black;
-  border-radius: 5px;
-  padding-left: 35px;
-  width: 250px;
-  height: 45px;
-  position: relative;
-`;
-const Logo = styled.h1`
-  font-weight: 600;
-`;
-const Anger = styled.a`
-  text-decoration: none;
-  color: #000000b3;
-`;
-const style = { position: "absolute", marginLeft: "5px" };
 function Navbar() {
   const quantity = useSelector(state => state.cart.quantity)
-  // console.log(quantity)
   return (
-    <Container>
-      <First>
-        <Anger href="shahilkv.com">Home</Anger>
-        <Anger href="shahilkv.com">Categories</Anger>
-        <Anger href="shahilkv.com">Contact</Anger>
-      </First>
-      <Second>
-        <Logo>
-          CLASSIC<span style={{ color: "red" }}>.</span>
-        </Logo>
-      </Second>
-      <Third>
-        <Input type="text" placeholder="Search" />
-        <Search style={style} />
-        <Link to="/cart">
-          <Button>
-            <Badge badgeContent={quantity} color="primary">
-              <ShoppingCartOutlined />
-            </Badge>
-          </Button>
-        </Link>
-      </Third>
-    </Container>
+    <div className="flex h-auto w-12/12 justify-center bg-white  items-center pt-10">
+
+      <div className=' flex flex-row gap-56 '>
+        <div className='flex flex-row w-1/4 gap-24'>
+          <a className='text-red-400   text-lg w-45' href="shahilkv.com">Categories</a>
+          <a className='text-red-400  text-lg w-45' href="shahilkv.com">Register</a>
+          <a className='text-red-400  text-lg w-45' href="shahilkv.com">Login</a>
+        </div>
+        <div>
+          <h1 className='w-56 font-semibold text-5xl'>CLASSIC<span style={{ color: "red" }}>.</span></h1>
+        </div>
+        <div className="w-2/4">
+          {/* <Search className="pl-48" /> */}
+          <input placeholder='Search here' className='rounded-lg border-black text-xl border-2 pl-4 p-2 w-64  mr-5' type="text" />
+          <Link to="/cart">
+            <button className='border-black bg-black text-white px-9 py-3 rounded-lg'>
+              <Badge badgeContent={quantity} color="primary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 }
 

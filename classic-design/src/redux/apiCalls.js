5dffaaeb22872ea414c1e4024643942ac78c6c40
shahil-leo <shahilcode@gmail.com>
2023-01-14 22:03:40 +0530

    added the login url

---
+++ classic-design/src/redux/apiCalls.js
@@ -0,0 +1,15 @@
+
+import { publicRequest } from '../requestMethod'
+import { loginFailure, loginStart, loginSuccess } from './userRedux'
+
+export const login = async (dispatch, user) => {
+    dispatch(loginStart())
+
+    try {
+        const res = await publicRequest.post("/auth/login", user)
+        dispatch(loginSuccess(res.data))
+    } catch (err) {
+        console.log(err)
+        dispatch(loginFailure())
+    }
+}

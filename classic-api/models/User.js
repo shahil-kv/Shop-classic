5e01280fac5f8fde0105e5a2454cadb194b5e5a5
shahil-leo <shahilcode@gmail.com>
2023-01-15 00:23:48 +0530

    added the img for the users

--- classic-api/models/User.js
+++ classic-api/models/User.js
@@ -1,20 +1,21 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 
-const userSchema=new mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
-        username:{type:String,required:true,unique:true},
-        email:{type:String,required:true,unique:true},
-        password:{type:String,required:true},
-        isAdmin:{
-            type:Boolean,
-            default:false,
-        } ,
+        username: { type: String, required: true, unique: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        img: { type: String }
 
-    }, {timestamps:true} )
+    }, { timestamps: true })
 
-    module.exports=mongoose.model('User',userSchema);
+module.exports = mongoose.model('User', userSchema);
 
     // definiction of what we are doing inside this we are creating a userSchema 
     // this defines what all things we need to be in the Userschema and i defined
-    // we need a username and email and password and there is is admin false 
+    // we need a username and email and password and there is is admin false
     // what all things we need if we want a user to 

d0d7fba5f8f5b461a8ee937b9c13e84b1e4418a5
shahil-leo <shahilcode@gmail.com>
2023-01-14 22:04:42 +0530

    added the reducers for the state to control

--- classic-design/src/redux/userRedux.js
+++ classic-design/src/redux/userRedux.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const userSlice = createSlice({
-    name: "cart",
+    name: "user",
     initialState: {
         currentUser: null,
         isFetching: false,

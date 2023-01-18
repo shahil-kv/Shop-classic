ca0bd8d5d5d4f92de0076543db23efe9835eef88
shahil-leo <shahilcode@gmail.com>
2023-01-18 09:13:04 +0530

    changed the styling from styled component to tailwind css in the cart section

--- classic-design/src/pages/Cart.jsx
+++ classic-design/src/pages/Cart.jsx
@@ -12,127 +12,24 @@ import { useNavigate } from 'react-router-dom'
 
 
 
-const TopButton = styled.button`
-    padding: 10px;
-    font-weight: 600;
-    cursor: pointer;
-    border: ${props => props.type === 'filled' && 'none'};
-    background-color: ${props => props.type === 'filled' ? 'black' : 'transparent'};
-    color: ${props => props.type === 'filled' && 'white'};
- `
-const TopTexts = styled.div`
-   
- `
-const TopText = styled.div`
-    text-decoration: underline;
-    cursor: pointer;
-    margin: 0px 10px;
-    
- `;
 
-const Bottom = styled.div`
-    display: flex;
-    justify-content: space-between;
-`
 const Info = styled.div`
-   flex: 3;
-`
-
-const Product = styled.div`
-  display: flex;
-  justify-content: space-between;
+   flex:3;
 `
 const ProductDetail = styled.div`
  flex: 2;
  display: flex ;
 `
-const Image = styled.img`
-  width: 200px;
-  `
-const ProductAmountContainer = styled.div`
-   display: flex;
-   align-items: center;
-  margin-bottom: 20px;
-  `
-const ProductAmount = styled.div`
-   font-size: 24px;
-   margin: 5px;
 
-  `
-const ProductPrice = styled.div`
-   font-size: 30px;
-   font-weight: 200;
-  `
-const Details = styled.div`
- padding: 20px;
- display: flex;
- flex-direction: column;
-  height: 200px;
- justify-content: space-around;
- `
-const ProductName = styled.span``
-const ProductId = styled.span``
-const ProductColor = styled.div`
-   width: 20px;
-   height: 20px;
-   border-radius: 50px;
-   background-color: black;
-  `
-const ProductSize = styled.span``
-const PriceDetail = styled.span`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
- flex: 1;
-`
-const Hr = styled.hr`
-   background-color: #eee;
-   border: none;
-   height: 1px;
-  `
-const SummaryTitle = styled.h1`
-    font-weight: 200;
 
-`
-const SummaryItem = styled.div`
-    margin: 30px 0px;
-    display: flex;
-    justify-content: space-between;
-    font-weight: 200;
-    font-size: 24px;
-`
-const SummaryItemText = styled.span`
-   
-`
-const SummaryItemPrice = styled.span`
-   
-`
 
-const Top = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 20px;
-    flex-direction: row;
- `
-// const Button = styled.button`
-//     width: 100%;
-//     padding: 10px;
-//     background-color: black;
-//     color: white;
-//     font-weight: 600;
-// `
-const Summary = styled.div`
-    flex: 1;
-    border: 0.5px solid black;
-    border-radius: 10px;
-    padding: 20px;
-    height: 50vh;
-`
-const Flex = styled.div`
-   display: flex;
-`
+
+
+
+
+
+
+
 
 const Cart = () => {
    const cart = useSelector(state => state.cart)
@@ -162,59 +59,59 @@ const Cart = () => {
       <div>
          <div className='p-20'>
             <div className='font-medium text-4xl text-center'>YOUR BAG</div>
-            <Top>
-               <TopButton type='filled'>CONTINUE SHOPPING</TopButton>
-               <TopTexts>
-                  <Flex>
-                     <TopText>Shopping Bag(2)</TopText>
-                     <TopText>Your Wishlist(0)</TopText>
-                  </Flex>
-               </TopTexts>
-               <TopButton>CHECKOUT NOW</TopButton>
-            </Top>
-            <Bottom>
+            <div className='flex  justify-between items-center p-5 flex-row ' >
+               <button type='filled' className='p-3 font-semibold cursor-pointer bg-black text-white'>CONTINUE SHOPPING</button>
+               <div>
+                  <div className='flex'>
+                     {/* <button className='p-3 font-semibold cursor-pointer bg-black text-white'>CHECKOUT NOW</button> */}
+                     <div className='underline cursor-pointer ml-3 mr-3'>Shopping Bag(2)</div>
+                     <div className='underline cursor-pointer ml-3 mr-3'>Your Wishlist(0)</div>
+                  </div>
+               </div>
+            </div>
+            <div className='flex justify-between'>
                <Info>
-                  <Hr />
+                  <hr className='bg-white border-none h-1' />
                   {cart.products.map(product => (
-                     <Product>
+                     <div className='flex justify-between' >
                         <ProductDetail>
-                           <Image src={product.img} />
-                           <Details>
-                              <ProductName><b>Product:</b>{product.title}</ProductName>
-                              <ProductId><b>ID</b>{product._id}</ProductId>
-                              <ProductColor color={product.color} />
-                              <ProductSize><b>Size:</b>{product.size}</ProductSize>
-                           </Details>
+                           <img className='w-80' alt='imgCart' src={product.img} />
+                           <div className='p-5 flex flex-col h-52 justify-around'>
+                              <span><b>Product:</b>{product.title}</span>
+                              <span><b>ID</b>{product._id}</span>
+                              <div className='w-5 h-5 rounded-3xl bg-black' color={product.color} />
+                              <span><b>Size:</b>{product.size}</span>
+                           </div>
                         </ProductDetail>
-                        <PriceDetail>
-                           <ProductAmountContainer>
+                        <span className='flex items-center flex-col justify-center flex-1'>
+                           <div className='flex items-center mb-5'>
                               <Add />
-                              <ProductAmount>{product.quantity}</ProductAmount>
+                              <div className='text-2xl m-2'>{product.quantity}</div>
                               <Remove />
-                           </ProductAmountContainer>
-                           <ProductPrice>{product.price * product.quantity}</ProductPrice>
-                        </PriceDetail>
-                     </Product>
+                           </div>
+                           <div className='text-3xl font-light'>{product.price * product.quantity}</div>
+                        </span>
+                     </div>
                   ))}
                </Info>
-               <Summary>
-                  <SummaryTitle> ORDER SUMMARY</SummaryTitle>
-                  <SummaryItem>
-                     <SummaryItemText>Subtotal</SummaryItemText>
-                     <SummaryItemPrice>Rs{cart.total}</SummaryItemPrice>
-                  </SummaryItem>
-                  <SummaryItem>
-                     <SummaryItemText>Estimated Shipping</SummaryItemText>
-                     <SummaryItemPrice>$5.90</SummaryItemPrice>
-                  </SummaryItem>
-                  <SummaryItem>
-                     <SummaryItemText>Shipping Discount</SummaryItemText>
-                     <SummaryItemPrice>-$5.99</SummaryItemPrice>
-                  </SummaryItem>
-                  <SummaryItem>
-                     <SummaryItemText type='total'>Total</SummaryItemText>
-                     <SummaryItemPrice>{cart.total}</SummaryItemPrice>
-                  </SummaryItem>
+               <div className='flex-1 border-2 border-black p-5 h-4/4'>
+                  <h1 className='font-normal'> ORDER SUMMARY</h1>
+                  <div className='flex justify-between font-extralight text-2xl mt-6'>
+                     <span>Subtotal</span>
+                     <span>Rs{cart.total}</span>
+                  </div>
+                  <div className='flex justify-between font-extralight text-2xl mt-6'>
+                     <span>Estimated Shipping</span>
+                     <span>$5.90</span>
+                  </div>
+                  <div className='flex justify-between font-extralight text-2xl mt-6'>
+                     <span>Shipping Discount</span>
+                     <span>-$5.99</span>
+                  </div>
+                  <div className='flex justify-between font-extralight text-2xl mt-6 '>
+                     <span type='total'>Total</span>
+                     <span>{cart.total}</span>
+                  </div>
                   <StripeCheckout
                      name='Classic Shop'
                      image=''
@@ -228,8 +125,8 @@ const Cart = () => {
                   >
                   </StripeCheckout>
 
-               </Summary>
-            </Bottom>
+               </div>
+            </div>
          </div>
          <Footer />
       </div>
